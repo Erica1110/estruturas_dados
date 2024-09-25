abf7cf807114988b2e96a2a0a9241cad8182e82b
Erica_Ferreira <89529255+Erica1110@users.noreply.github.com>
2024-09-25 00:09:44 -0300

    Update Pilha.kt

--- Pilha.kt
+++ Pilha.kt
@@ -1,13 +1,53 @@
 class Pilha: Empilhavel {
-    //val é uma variavel que nao pode ser mudada
-    private val ponteiroTopo: Int
-    //se uma variavel nao recebe atributo, ele tera que ser iniciada com sue tipo
-    //var é uma variavel cque pode ser alterada no decorrer do codigo
-    private var dados: Array<Any>
+    private val elementos: Array<Any?>
+    private var indice: Int
+    constructor(tamanho: Int = 10) {
+        elementos = arrayOfNulls(tamanho)
+        indice = -1
+    }
 
+    override fun estaCheia(): Boolean {
+        return indice == elementos.size - 1
+    }
 
-    contrutor(tamanho; Int){
-        PonteiroTopo = -1
-        dados = arrayOfNull(tamanho)
+    override fun estaVazia(): Boolean {
+        return indice ==  -1
+    }
+
+    override fun desempilhar(): Any? {
+            var aux: Any? = null
+        if (!estaCheia()) aux = elementos[indice]
+       indice--
+        return aux
+    }
+
+    override fun atualizar(value: Any?) {
+            if (!estaVazia()) {
+            elementos[indice] = value;
+          } else {
+            println("Stack is empty")
+        }
+    }
+
+    override fun empilhar(value: Any?) {
+            if (!estaCheia()) {
+            indice++
+            elementos[indice] = value;
+        } else {
+            println("Stack is full")
+        }
+    }
+
+    fun imprimir(): String {
+        var retorno = "["
+            for (i in indice downTo 0) {
+            retorno += if (i == 0) {
+            "${elementos[i]}"
+        } else {
+            "${elementos[i]},"
+            }
+
+        }
+        return "$retorno]"
     }
 }

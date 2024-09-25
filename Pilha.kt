11faac7aa11d92821c79159677e7101f12d2c17d
Erica_Ferreira <89529255+Erica1110@users.noreply.github.com>
2024-09-24 22:37:03 -0300

    Update Pilha.kt

--- Pilha.kt
+++ Pilha.kt
@@ -1,5 +1,8 @@
 class Pilha: Empilhavel {
+    //val é uma variavel que nao pode ser mudada
     private val ponteiroTopo: Int
+    //se uma variavel nao recebe atributo, ele tera que ser iniciada com sue tipo
+    //var é uma variavel cque pode ser alterada no decorrer do codigo
     private var dados: Array<Any>
 
 

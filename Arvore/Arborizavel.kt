54d27c0d5e20d83f1f45a043153564c028e077a3
erica <ericameirepratesferreira@gmail.com>
2024-12-14 15:06:30 -0300

    Arvore

---
+++ Arvore/Arborizavel.kt
@@ -0,0 +1,16 @@
+package Arvore
+
+interface Arborizavel {
+    fun inserir(dado: Any?)
+    fun apagar(dado: Any?): Boolean
+    fun limpar()
+    fun existe(dado: Any?) : Boolean
+    fun buscar(dado: Any?) : 
+
+    fun preOrdem() : String
+    fun emOrdem(): String
+    fun posOrdem() : String
+
+    )
+
+}
